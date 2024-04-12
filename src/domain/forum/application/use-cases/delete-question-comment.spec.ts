2c30451c5693a2ce617dbe463281608b8abf2ce2
Gabriel Ramalho <biel-pedro@hotmail.com>
2024-04-11 21:48:04 -0300

    feat: create delete answer comment use case

--- src/domain/forum/application/use-cases/delete-question-comment.spec.ts
+++ src/domain/forum/application/use-cases/delete-question-comment.spec.ts
@@ -1,6 +1,6 @@
 import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository'
 import { DeleteQuestionCommentUseCase } from './delete-question-comment'
-import { makeQuestionComment } from 'test/factories/make-quesiton-comment'
+import { makeQuestionComment } from 'test/factories/make-question-comment'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
